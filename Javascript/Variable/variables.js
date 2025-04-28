1c477b97c8bc8d97d4492194e497c542f2c9d017
helder Ribeiro <Helderlagrisola@gmail.com>
2025-04-28 21:23:28 +0100

    Variables

---
+++ Javascript/Variable/variables.js
@@ -0,0 +1,19 @@
+
+//In JavaScript, variables are used to store and manage data within a program. 
+// They act as containers that hold different types of information, such as numbers, text, or objects, and allow you to manipulate and work with that data in your code.
+
+
+
+
+var variables = "this is variable var"
+let variableLet = "this is variable let"
+const variableConst = "this is const variable"
+
+
+
+
+//Print to console section
+console.log(variableLet)
+console.log(variables)
+console.log(variableConst)
+
