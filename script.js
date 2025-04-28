cae2388d21b486b100b91b6860a49fe732d100ce
helder Ribeiro <Helderlagrisola@gmail.com>
2025-04-28 16:29:47 +0100

    Started with some Primitive Data Types

---
+++ script.js
@@ -0,0 +1,12 @@
+//JavaScript data types can be broadly categorized into primitive and non-primitive types. 
+// Primitive types include String, Number, Boolean, Undefined, Null, Symbol, and BigInt.
+//  Non-primitive types include Object, Array, and Function.
+//  Understanding these data types is crucial for effective programming in JavaScript.
+
+//Primitive Data Types
+let String = "Hello World" // This is string data type
+let number = 2; //This is number 
+let Boolean = true; //This is boolean data type : it's either true or false 
+let Undefined = undefined; // This is undefined data type
+let Null =  null; // This is null data type
+console.log(String,number,Boolean,Undefined,Null);
